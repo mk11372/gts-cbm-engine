8d1205325354cc139264b4580f587c21f0374c65
Mike <michael.kerpelman@getfabric.com>
2021-12-06 06:32:32 -0500

    Default class attributes for numerical values set to 0

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pandas as pd
-import csv
+import snowflake.connector
 
 class Asset:
 
     def __init__(self, name):
         self.name = name
+
+    def open_snowflake_connection(self):
+
+        # Configuration parameters
+        cnn = snowflake.connector.connect (
+        user='MICHAELK',
+        password='nEKZUgX4wy7bUj6',
+        account='fa82594.us-central1.gcp',
+        warehouse='CSR_DB_LOAD',
+        database='CSR_DB',
+        schema='CSR_RAW'
+        )
+
+        return cnn
 
 class TouchPoint(Asset):
 
@@ -25,21 +39,21 @@ class Elevator:
 
 class TouchPointElevator(TouchPoint, Elevator):
 
-    def __init__(self, name, side, total_cycles, last_maintenance_date, partial_cycles):
+    def __init__(self, name, side, total_cycles=0, last_maintenance_date=0, partial_cycles=0):
         TouchPoint.__init__(self, name)
         Elevator.__init__(self, total_cycles, last_maintenance_date, partial_cycles)
         self.side = side
 
 class TerminalElevator(TouchPoint, Elevator):
 
-    def __init__(self, name, total_cycles, last_maintenance_date, partial_cycles):
+    def __init__(self, name, total_cycles=0, last_maintenance_date=0, partial_cycles=0):
         Terminal.__init__(self, name)
         Elevator.__init__(self, total_cycles, last_maintenance_date, partial_cycles)
 
-assets = pd.read_csv("assets.csv", header=0)
+# assets = pd.read_csv("assets.csv", header=0)
 
-for i in assets.index:
-    if "tp" in assets.name[i]:
-        print("tp")
-    else:
-        print("no tp")
+# for i in assets.index:
+#     if "tp" in assets.name[i]:
+#         TouchPointElevator(assets.name[i])
+#     else:
+
