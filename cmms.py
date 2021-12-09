88d4b7d249abce8aa7454e8f64ad15fbcd3099df
Mike <michael.kerpelman@getfabric.com>
2021-12-09 08:01:23 -0500

    Enable service call pull from Priority API

--- cmms.py
+++ cmms.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 # ==== Manage connection to Priority API ====
 
@@ -6,4 +7,11 @@ def fetch_raw_data():
     r = requests.get("https://csense.wee.co.il/odata/Priority/tabula.ini/csusain/DOCUMENTS_Q", auth=('API', 'Ap123456!'))
     return r.json()
 
+def raw_to_df(raw_json):
+    return pd.DataFrame(raw_json['value'])
 
+def run_api_pull():
+    service_calls = fetch_raw_data()
+    print(raw_to_df(service_calls).head(40))
+
+run_api_pull()
